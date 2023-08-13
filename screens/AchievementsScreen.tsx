695de7a88ee510142ce9c95d21cdfb609ce2f16d
Lucas Kim <lucaskim@Lucass-Mac-mini.local>
2023-08-13 18:45:19 -0400

    Add achievements page

--- screens/AchievementsScreen.tsx
+++ screens/AchievementsScreen.tsx
@@ -16,46 +16,70 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Achievements'> & {
 const achievements = [
   {
     name: 'Dreamland Buddies',
-    description: 'Collect two common sleep creatures to prove you\'re building a cozy sleep environment.'
+    description: 'Collect two common sleep creatures to prove you\'re building a cozy sleep environment.',
+    rarity: 'Common'
   },
   {
     name: 'Snooze Companions',
-    description: 'Form a bond with a pair of uncommon sleep creatures and enjoy even more restful nights.'
+    description: 'Form a bond with a pair of uncommon sleep creatures and enjoy even more restful nights.',
+    rarity: 'Uncommon',
   },
   {
     name: 'Rested Relations',
-    description: 'Forge a strong connection with two rare sleep creatures, enhancing your sleep quality.'
+    description: 'Forge a strong connection with two rare sleep creatures, enhancing your sleep quality.',
+    rarity: 'Rare',
   },
   {
     name: 'Dream Control Master',
-    description: 'Harness the power of two epic sleep creatures, showcasing your mastery over the dream realm.'
+    description: 'Harness the power of two epic sleep creatures, showcasing your mastery over the dream realm.',
+    rarity: 'Epic',
   },
   {
     name: 'Legends of Slumber',
-    description: 'Unite two legendary sleep creatures, solidifying your status as a legend in the world of dreams.'
+    description: 'Unite two legendary sleep creatures, solidifying your status as a legend in the world of dreams.',
+    rarity: 'Legendary'
   },
   {
     name: 'Mythic Dreamweaver',
-    description: 'Attain a harmonious pair of mythic sleep creatures, elevating your dream experiences to the mythic realm.'
+    description: 'Attain a harmonious pair of mythic sleep creatures, elevating your dream experiences to the mythic realm.',
+    rarity: 'Mythic'
   },
   {
     name: 'Divine Dream Guardians',
-    description: 'Prove your dedication by acquiring two divine sleep creatures, establishing an unbreakable dream bond.'
+    description: 'Prove your dedication by acquiring two divine sleep creatures, establishing an unbreakable dream bond.',
+    rarity: 'Divine',
   },
   {
     name: 'Celestial Harmony',
-    description: 'Achieve perfect harmony with two celestial sleep creatures, reaching the highest echelons of dream mastery.'
+    description: 'Achieve perfect harmony with two celestial sleep creatures, reaching the highest echelons of dream mastery.',
+    rarity: 'Celestial'
   },
 ];
+
+// const rarityIndices = {Common: 0, Uncommon: 1, Rare: 2, Epic: 3, Legendary: 4, Mythic: 5, Divine: 6, Celestial: 7};
 
 function AchievementsScreen({ creatures, dispatch, route }: Props): JSX.Element {
   return (
     <FlatList
       data={achievements}
-      renderItem={({ item }) => (
-        <View>
-          <Text style={styles.achievementName}>{item.name}</Text>
-        </View>
+      renderItem={({ item, index }) => (
+        creatures.creatures[index * 2].owned && creatures.creatures[index * 2 + 1].owned ? (
+          <View style={styles.achievementContainer}>
+            <Image style={styles.achievementImage} source={require('../assets/phoenix.png')} />
+            <View style={styles.achievementTextContainer}>
+              <Text style={styles.achievementName}>{item.name}</Text>
+              <Text style={styles.achievementDescription}>{item.description}</Text>
+            </View>
+          </View>
+        ) : (
+          <View style={styles.achievementContainer}>
+            <Image style={styles.achievementImage} source={require('../assets/phoenix.png')} />
+            <View style={styles.achievementTextContainer}>
+              <Text style={styles.lockedName}>{item.name}</Text>
+              <Text style={styles.lockedDescription}>Not unlocked yet</Text>
+            </View>
+          </View>
+        )
       )}
     />
   )
@@ -63,10 +87,40 @@ function AchievementsScreen({ creatures, dispatch, route }: Props): JSX.Element 
 
 const styles = StyleSheet.create({
   achievementContainer: {
-    
+    flex: 1,
+    flexDirection: 'row',
+    padding: 10,
+    width: '100%'
+  },
+  achievementImage: {
+    width: 100,
+    height: 100
+  },
+  achievementTextContainer: {
+    paddingLeft: 20,
+    paddingRight: 100
   },
   achievementName: {
-    color: '#FFFFFF'
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  achievementDescription: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    flexShrink: 1,
+  },
+  lockedName: {
+    color: '#AAAAAA',
+    fontSize: 18,
+    flexShrink: 1,
+    fontStyle: 'italic'
+  },
+  lockedDescription: {
+    color: '#AAAAAA',
+    fontSize: 16,
+    flexShrink: 1,
+    fontStyle: 'italic',
   }
 });
 
